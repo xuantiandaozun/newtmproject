61997c4579e1515b39bc343f4e475cc10a84f37e
zhoudingwen <18081047182@163.com>
2019-01-18 20:16:55 +0800

    no

--- tianmayunxi02/src/main/java/com/system/tianmayunxi/zp02yx_xzmbh/TmyxRouterConfig.java
+++ tianmayunxi02/src/main/java/com/system/tianmayunxi/zp02yx_xzmbh/TmyxRouterConfig.java
@@ -1,29 +1,28 @@
 package com.system.tianmayunxi.zp02yx_xzmbh;
 
 public class TmyxRouterConfig {
-    public static final String MAIN_FRAGMENT = "/main/fragment";
-
-    public static final String TMYX_GFTJ="/tmyx/gftj";
-    public static final String TMYX_TIDETAIL="/tmyx/tidetail";
-    public static final String TMYX_REPORT="/tmyx/report";
-    public static final String TMYX_PLLIST="/tmyx/plist";
-    public static final String TMYX_PLALL="/tmyx/plall";
-    public static final String TMYX_WDDY="/tmyx/wddy";
-    public static final String TMYX_CKIMAGE="/tmyx/ckimage";
-    public static final String TMYX_QBZT ="/tmyx/qbzt";
-    public static final String TMYX_XZZT ="/tmyx/xzzt";
-    public static final String TMYX_ADDZT ="/tmyx/addzt";
-    public static final String TMYX_THEMEDETAIL ="/tmyx/themedetail";
-    public static final String TMYX_WDFB="/tmyx/wdfb";
-    public static final String TMYX_LQJF="/tmyx/lqjf";
-    public static final String TMYX_SIGNRULE="/tmyx/signrule";
-    public static final String TMYX_DHJG="/tmyx/dhjg";
-    public static final String LQJF_INFO="/tmyx/info";
-    public static final String LQJF_ACCOUNT="/tmyx/account";
-    public static final String LQJF_BINDPHONE="/tmyx/bindphone";
-    public static final String LQJF_JFSHOP="/tmyx/jfshop";
-    public static final String LQJF_ADDADDRESS="/tmyx/addaddress";
-    public static final String LQJF_SHOPDETAIL="/tmyx/shopdetail";
-    public static final String WDFB_DYNAMIC="/wdfb/dynamic";
-    public static final String WDFB_MESSAGE="/wdfb/message";
+    public static final String MAIN_FRAGMENT = "/main02/fragment";
+    public static final String TMYX_GFTJ="/tmyx02/gftj";
+    public static final String TMYX_TIDETAIL="/tmyx02/tidetail";
+    public static final String TMYX_REPORT="/tmyx02/report";
+    public static final String TMYX_PLLIST="/tmyx02/plist";
+    public static final String TMYX_PLALL="/tmyx02/plall";
+    public static final String TMYX_WDDY="/tmyx02/wddy";
+    public static final String TMYX_CKIMAGE="/tmyx02/ckimage";
+    public static final String TMYX_QBZT ="/tmyx02/qbzt";
+    public static final String TMYX_XZZT ="/tmyx02/xzzt";
+    public static final String TMYX_ADDZT ="/tmyx02/addzt";
+    public static final String TMYX_THEMEDETAIL ="/tmyx02/themedetail";
+    public static final String TMYX_WDFB="/tmyx02/wdfb";
+    public static final String TMYX_LQJF="/tmyx02/lqjf";
+    public static final String TMYX_SIGNRULE="/tmyx02/signrule";
+    public static final String TMYX_DHJG="/tmyx02/dhjg";
+    public static final String LQJF_INFO="/tmyx02/info";
+    public static final String LQJF_ACCOUNT="/tmyx02/account";
+    public static final String LQJF_BINDPHONE="/tmyx02/bindphone";
+    public static final String LQJF_JFSHOP="/tmyx02/jfshop";
+    public static final String LQJF_ADDADDRESS="/tmyx02/addaddress";
+    public static final String LQJF_SHOPDETAIL="/tmyx02/shopdetail";
+    public static final String WDFB_DYNAMIC="/wdfb02/dynamic";
+    public static final String WDFB_MESSAGE="/wdfb02/message";
 }
