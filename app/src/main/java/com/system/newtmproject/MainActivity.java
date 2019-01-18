1a05e2202d4b3a34e7aa05084c0a219f9cd71352
zhou <18081047182@163.com>
2019-01-18 17:33:31 +0800

    初始化

---
+++ app/src/main/java/com/system/newtmproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.system.newtmproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
